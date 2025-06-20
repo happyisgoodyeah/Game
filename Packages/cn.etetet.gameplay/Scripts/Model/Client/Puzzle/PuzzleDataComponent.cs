178696fe20841f8208b6b188a41739423a2560c0
behappy <1208712478@qq.com>
2025-06-20 21:15:46 +0800

    puzzle改为直接生成在grid 代码逻辑改为获取对应位置 方便grid内调整对应puzzle位置

--- Packages/cn.etetet.gameplay/Scripts/Model/Client/Puzzle/PuzzleDataComponent.cs
+++
@@ -1,9 +0,0 @@
-namespace ET
-{
-    [ComponentOf(typeof(Puzzle))]
-    public class PuzzleDataComponent : Entity , IAwake<IntVector2,int>
-    {
-        public IntVector2 puzzlePosition; // 逻辑坐标
-        public int rotation;
-    }
-}
